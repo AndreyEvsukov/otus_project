b1a0000f0c471ba8f9883aaf3e67f08e53247796
evsukov <evsukov@nsd.ru>
2025-08-29 20:46:57 +0300

    del

--- src/main/java/ru/project_bot/finance/api/moex/MoexApiClient.java
+++
@@ -1,9 +0,0 @@
-package ru.project_bot.finance.api.moex;
-
-import ru.project_bot.finance.api.moex.model.MxdResponse;
-import ru.project_bot.finance.api.moex.model.ShareData;
-
-public interface MoexApiClient {
-    MxdResponse getSecurities();
-    ShareData getSharePrice(String ticker);
-}
